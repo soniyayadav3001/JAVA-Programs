7105fd15b4864a372d81b5e793d74980579141d7
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-08 19:10:02 +0530

    Create table_of_any_number.java

---
+++ table_of_any_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class hello {
+    public static void main(String[] args) {
+        int i;
+        System.out.print("Enter the value of 'n' : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (i = 1; i <= 10;i++) {
+            System.out.println(n*i);
+        }
+       
+    }
+
+}
