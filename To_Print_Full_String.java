d7a99b3e759c0b1cfe1c855ed0457280cf7c219c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-09 17:28:21 +0530

    Create To_Print_Full_String.java

---
+++ To_Print_Full_String.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.println("Enter your string here : ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+
+    }
+
+}
