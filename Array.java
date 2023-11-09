1bcba3388e2ab879d76664779b4e01dc8420240c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-09 17:35:34 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int marks[] = { 98, 96, 99, 95 };
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
