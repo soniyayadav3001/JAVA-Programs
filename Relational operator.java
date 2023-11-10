e6049cb31db31308c28ef0fb18f934438735936c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-10 19:16:14 +0530

    Create Relational operator.java

---
+++ Relational operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of a here : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b here : ");
+        Scanner s = new Scanner(System.in);
+        int b = s.nextInt();
+        System.out.println("Are both values equal ?");
+        System.out.println(a == b);
+        
+    }
+
+}
