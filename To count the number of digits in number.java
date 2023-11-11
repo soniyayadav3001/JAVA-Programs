988c3575b423fbf177ac3ec0d38f32a60385a7fb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 13:45:57 +0530

    Create To count the number of digits in number.java

---
+++ To count the number of digits in number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int count = 0;
+        System.out.println("Enter the number here : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        while (num > 0) {
+            num = num / 10;
+            count = count + 1;
+        }
+        System.out.println(count);
+    }
+}
