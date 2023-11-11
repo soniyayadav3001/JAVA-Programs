1dd4e4745732f61b097eeb56b8fd12b081d63ed7
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 13:44:45 +0530

    Create Sum of numbers.java

---
+++ Sum of numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.println("Enter the number here : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum=0;
+        for (int i = 1; i <= num; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum will be : ");
+        System.out.println(sum);
+
+    }
+
+}
