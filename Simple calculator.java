3e529b526102f0b53eb90da6ed528ca504b358c6
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 14:48:19 +0530

    Create Simple calculator.java

---
+++ Simple calculator.java
@@ -0,0 +1,32 @@
+//TASK- TO MAKE A CALCULATOR
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        System.out.println("Enter the value of num1 : ");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println("Enter the value of num2 : ");
+        Scanner s = new Scanner(System.in);
+        int num2 = s.nextInt();
+        System.out.println("Enter the operator here : ");
+        Scanner scr = new Scanner(System.in);
+        char op = scr.next().charAt(0);
+          switch(op){
+              case '+' :
+                  System.out.println(num1+num2);
+                  break;
+              case '-' :
+                  System.out.println(num1-num2);
+                  break;
+              case '*' :
+                  System.out.println(num1*num2);
+                  break;
+              case '/' :
+                  System.out.println(num1/num2);
+                  break;
+              default:
+                  System.out.println("Invalid operator");
+          }
+    }
+
+}
