e50b186e0e5a3f6b4a874436f24228272cf70b69
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-22 18:44:12 +0530

    Create Even and Odd.java

---
+++ Even and Odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("Number is even.");
+        }
+        else {
+            System.out.println("Number is odd.");
+        }
+    }
+}
