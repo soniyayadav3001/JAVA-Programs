1a26484b90dbf5313c15f50c4698942cdc1ee274
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 19:27:43 +0530

    Create pH value tester.java

---
+++ pH value tester.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class lab {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO CHEM LAB TESTING");
+        System.out.println("Enter the pH value of chemical to be tested : ");
+        Scanner sc = new Scanner(System.in);
+        float pH = sc.nextFloat();
+        if (pH > 7.0) {
+            System.out.println("CHEMICAL IS BASIC");
+        } else if (pH < 7.0) {
+            System.out.println("CHEMICAL IS ACIDIC");
+        } else {
+            System.out.println("CHEMICAL IS NEUTRAL");
+        }
+        System.out.println("THANK YOU");
+    }
+}
