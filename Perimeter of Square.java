8e802702a525cfbd1633198c758ee669c0b1e4da
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 19:42:34 +0530

    Create Perimeter of Square.java

---
+++ Perimeter of Square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        //Perimeter of square.
+        System.out.println("Enter the value 'a' : ");
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.println("The perimeter of square is : "+4*a);
+    }
+}
