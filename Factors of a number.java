30849909ecfc7b804190128c9745ff3f2a16ee38
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 20:17:57 +0530

    Create Factors of a number.java

---
+++ Factors of a number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Factor {
+  public static void main(String[] args) {
+    System.out.println("Enter the number here : ");
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    System.out.print("Factors of " + number + " are: ");
+
+    for (int i = 1; i <= number; ++i) {
+
+      // if number is divided by i
+      // i is the factor
+      if (number % i == 0) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
