4c02f8a34b73ea4a2d71af8eece7ab6ed1b2fd3f
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 19:39:50 +0530

    Create Perimeter of Rectangle.java

---
+++ Perimeter of Rectangle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        //Perimeter of rectangle.
+        System.out.println("Enter the value of 'l' and 'b' : ");
+        Scanner sc = new Scanner(System.in);
+        float l = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.println("The perimeter of rectangle is : "+2*(l+b));
+    }
+}
