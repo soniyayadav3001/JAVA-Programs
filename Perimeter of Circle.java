14debe17a9127957d7b7eefbe54add2701bce04a
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 19:44:46 +0530

    Create Perimeter of Circle.java

---
+++ Perimeter of Circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        //Perimeter of circle.
+        System.out.println("Enter the value radius : ");
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        System.out.println("The perimeter of circle is : "+2*3.14*r);
+    }
+}
