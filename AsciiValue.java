11f7785ed06845a17eb51c0a791ba5b9275fb29b
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-23 20:05:58 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,9 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+
+        int ch = 'A';
+
+        System.out.println("The ASCII value of " + ch + " is: " + ch);
+    }
+
+}
