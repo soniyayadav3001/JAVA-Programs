83d294ba8dade780037b53326ca6b763aed3fb01
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-24 18:25:42 +0530

    Create Convert double to int using Typecasting.java

---
+++ Convert double to int using Typecasting.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // create double variables
+    double a = 23.78;
+    double b = 52.11;
+
+    // convert double into int
+    // using typecasting
+    int c = (int)a;
+    int d = (int)b;
+
+    System.out.println(c);    // 23
+    System.out.println(d);    // 52
+  }
+}
