14c2c302ca3c062a4e4235c671f561e2e3fb9f0f
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-10 20:08:41 +0530

    Update FahrenheitToCelsius.java

--- FahrenheitToCelsius.java
+++ FahrenheitToCelsius.java
@@ -1,21 +1,11 @@
 import java.util.*;
-
 public class Temperature {
-
   public static void main(String[] args) {
-
     float temperature;
-
     Scanner in = new Scanner(System.in);
-
     System.out.println("Enter temperature in Fahrenheit");
-
     temperature = in.nextInt();
-
     temperature = ((temperature - 32)*5)/9;
-
     System.out.println("temperature in Celsius = " + temperature);
-
   }
-
 }
