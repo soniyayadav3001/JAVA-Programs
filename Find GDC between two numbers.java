a7998004519443730ed7bd383a7b62d18569e606
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-22 19:36:12 +0530

    Update Find GDC between two numbers.java

--- Find GDC between two numbers.java
+++ Find GDC between two numbers.java
@@ -5,7 +5,6 @@ public class Main {
     // initially set to gcd
     int gcd = 1;
     for (int i = 1; i <= n1 && i <= n2; ++i) {
-
       // check if i perfectly divides both n1 and n2
       if (n1 % i == 0 && n2 % i == 0)
         gcd = i;
