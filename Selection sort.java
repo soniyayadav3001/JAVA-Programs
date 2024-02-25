2446d5a940839451928e5c29da6638b1bc0c039d
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-02-25 21:44:36 +0530

    Create Selection sort.java

---
+++ Selection sort.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]+" ");
+        }
+    } 
+    public static void main(String[] args) {
+        //Selection Sort
+        int arr[] = { 7, 8, 3, 1, 2 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest=j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    } 
+}
