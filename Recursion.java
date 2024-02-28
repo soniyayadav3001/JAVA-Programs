11a8778eebf9a311e3cb74accebc17ceacc361d7
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-02-28 21:45:10 +0530

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static void printnum(int num) {
+        if (num == 6) {
+            return;
+        }
+        System.out.println(num);
+        printnum(num+1);
+    }
+    public static void main(String[] args) {
+        int num = 1;
+        printnum(num);
+    }
+}
