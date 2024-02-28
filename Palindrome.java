9494ff05a64ff0c9708829bc8d730d2258678dc0
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-02-28 18:26:22 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+public static void main(String[]args){
+    int original_num, reverse_num = 0, remainder;
+    System.out.println("Enter the number here : ");
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    original_num = num;
+    while (num > 0) {
+        remainder = num % 10;
+        reverse_num = reverse_num * 10 + remainder;
+        num = num / 10;
+    }
+    if (original_num == reverse_num) {
+        System.out.println("Number is Palindrome");
+    }
+    else {
+        System.out.println("Number is not Palindrome");
+    }
+}
+}
