ed906f077d277ab9f11c29924d987f97b4c01b51
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-04-27 21:34:20 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Factorial {
-    
     public static void main(String[] args) {
         System.out.println("Enter the number here : ");
         Scanner sc = new Scanner(System.in);
