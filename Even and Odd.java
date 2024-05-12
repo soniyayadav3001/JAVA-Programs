b6d470969645fb8ca5303b9dab3c5c33b343813c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-05-12 14:54:43 +0530

    Update Even and Odd.java

--- Even and Odd.java
+++ Even and Odd.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class hello {
+    
     public static void main(String[] args) {
         System.out.println("Enter the number : ");
         Scanner sc = new Scanner(System.in);
