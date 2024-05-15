3e2c840aaf7c42b5fe904b28652f0559f86baefd
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-05-15 20:52:22 +0530

    Create TryCatch.java

---
+++ TryCatch.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Case1");
+        System.out.println("Case2");
+        System.out.println("Case3");
+        try{
+        int a =10/0;}
+        catch (Exception e){
+            System.out.println("Exception Handled");
+        }
+        System.out.println("Case4");
+        System.out.println("Case5");
+    }
+}
