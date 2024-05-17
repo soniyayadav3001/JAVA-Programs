d97913914a9cc0a120c2b01068eedd7d3ffca363
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-05-17 21:09:17 +0530

    Create Private_Access_Modifier.java

---
+++ Private_Access_Modifier.java
@@ -0,0 +1,10 @@
+public class Main {
+   private int counter = 30;
+    private int getdata(){
+        return counter;
+    }
+        public static void main(String[] args) {
+        Main obj = new Main();
+            System.out.println(obj.getdata());;
+    }
+}
