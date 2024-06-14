6a8993e74421e5f0d491b9070651ec12f6396637
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-06-14 15:15:46 +0530

    Update Check character is alphabet or not.java

--- Check character is alphabet or not.java
+++ Check character is alphabet or not.java
@@ -1,5 +1,5 @@
 public class Alphabet {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         char c = '*';
         if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
             System.out.println(c + " is an alphabet.");
