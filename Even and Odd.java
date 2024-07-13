077d8a2f0c13ad0d7520c72e6be3a78c0cb69594
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-07-13 21:15:00 +0530

    Update Even and Odd.java

--- Even and Odd.java
+++ Even and Odd.java
@@ -7,7 +7,7 @@ public class hello {
         int n = sc.nextInt();
         if (n % 2 == 0) {
             System.out.println("Number is even.");
-        }
+        } 
         else 
         {
             System.out.println("Number is odd.");
