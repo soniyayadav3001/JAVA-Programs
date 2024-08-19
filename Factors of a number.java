ee3843879f421d7e35babbdfea6f001d60a90c6a
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-08-19 23:27:44 +0530

    Update Factors of a number.java

--- Factors of a number.java
+++ Factors of a number.java
@@ -7,7 +7,6 @@ public class Factor {
     System.out.print("Factors of " + number + " are: ");
 
     for (int i = 1; i <= number; ++i) {
-
       // if number is divided by i
       // i is the factor
       if (number % i == 0) {
