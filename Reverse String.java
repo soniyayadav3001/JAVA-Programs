62202666afe4672fd0e28f5bfd2556115884b549
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-11-12 21:17:42 +0530

    Update Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -2,7 +2,6 @@ public class ReverseString {
     public static void main(String[] args) {
         String str = "Hello, World!";
         String reversedString = ""; 
-
         for (int i = str.length() - 1; i >= 0; i--) {
             reversedString += str.charAt(i);
         }
