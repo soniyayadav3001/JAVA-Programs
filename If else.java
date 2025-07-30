a1bc784785a6856f6c94e8d1cbd494d462b651c2
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2025-07-30 20:14:57 +0530

    Update If else.java

--- If else.java
+++ If else.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Main{
-
     public static void main(String[] args) {
         System.out.println("Enter the age here : ");
         Scanner sc = new Scanner(System.in);
